1caeb986e609e763388082d407c18f27fa2b6177
johnson434 <ghkddlsrb96@gmail.com>
2024-11-20 14:29:43 +0900

    EKS 클러스터 구축 추가

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 provider "aws" {
-    region = "ap-northeast-2"
+	region = "ap-northeast-2"
+}
+
+terraform {
+	required_version = ">= 1.0"
+
+	required_providers {
+		aws = {
+			source	= "hashicorp/aws"
+			version	= "~> 5.75"
+		}
+	}
 }
 
 module "vpc" {
